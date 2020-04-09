bb33931e6a7ded8e7d5282cb79555825a6417edb
Oleh Nevhad <nevgad777@gmail.com>
2020-04-09 16:16:09 +0300

    using setTimeout 2

--- setTimeout.js
+++ setTimeout.js
@@ -1,7 +1,17 @@
-
+/* Варинт №1 */
 var arr = [1, 2, 3, 4, 5];
 for (let i = 0; i < arr.length; i++){
   setTimeout(()=>{
      console.log(arr[i])
    }, (i + 1) * 1000);
+}
+
+/* Варинт №2 */
+var arr = [1, 2, 3, 4, 5];
+for ( var i = 0; i < arr.length; i++){
+setTimeout(function (item) {
+    return function () {
+    console.log(arr[item])
+  }
+}(i),(i + 1) * 1000)
 }
