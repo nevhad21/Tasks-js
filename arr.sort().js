b946c5747f7dde03f59ef518261b0a8d0aca17e6
Oleh Nevhad <nevgad777@gmail.com>
2020-04-27 20:32:48 +0300

    corect f.sort

--- arr.sort().js
+++ arr.sort().js
@@ -1,9 +1,10 @@
+
 var personalCompany = [2, 7, 4, 8, 78, 1];
 function sort(arr, callBack) {
   var n = arr.length;
     for (var i = n-1; i >= 0; i--) {
       for (var j = 1; j <= i; j++) {
-        if (arr[j-1] < arr[j) {
+        if (callBack(arr[j-1], arr[j])) {
         var t = arr[j-1];
         arr[j-1] = arr[j];
         arr[j] = t;
@@ -15,11 +16,11 @@ function sort(arr, callBack) {
 
 var result = sort(personalCompany, function(a, b) {
   if (a > b) {
-    return 1; 
-}
+    return 1;
+  }
   if (a < b) {
-    return 0; 
-}
+    return 0;
+  }
 
 });
-console.log(result);
+console.log(result
